d5edcecaf4b80daab2ed33719c49413d0a983743
Daniel Clarke <deliotclarke@gmail.com>
2019-07-06 15:58:47 -0500

    added a bunch of header stuff! including a typing intro. haha. i don't know man. it's fun.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,36 @@
 import React from 'react';
 import './App.css';
 
+import headerImg from './headerImg.jpg';
+
 function App() {
   return (
     <div className="App">
-      <header>Personal Site of Eliot Clarke</header>
+      <nav>
+        <div className="navbar">
+          <ul className="nav-links">
+            <li className="nav-item">Intro</li>
+            <li className="nav-item">About</li>
+            <li className="nav-item">Projects</li>
+            <li className="nav-item">Tech Stuff</li>
+            <li className="nav-item">Resume</li>
+            <li className="nav-item">More</li>
+          </ul>
+        </div>
+      </nav>
+      <section className="hero-section">
+        <div className="image-div">
+          <img className="headerImg" src={headerImg} alt="" />
+        </div>
+        <div className="intro-div">
+          <div className="intro-container">
+            <h2 className="intro">Hello, my name is Eliot Clarke.</h2>
+          </div>
+        </div>
+      </section>
+      <section className="main-body">
 
+      </section>
 
     </div>
   );
